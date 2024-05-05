daf8cb623f650bf6fba2c087673a09ea48a1c2d8
JainsL <117689545+JainsL@users.noreply.github.com>
2024-05-05 18:13:44 +0700

    Backend updated

--- backend/controllers/OrderCOntroller.js
+++ backend/controllers/OrderCOntroller.js
@@ -7,7 +7,7 @@ export const createOrder = async (req, res) => {
         const orderData = { // ID order yang Anda inginkan
             productId: '1', // ID produk yang terkait dengan order
             dateOrder: new Date(), // Tanggal pemesanan
-            orderStatus: 'Failed'
+            orderStatus: 'Pending'
             // Anda dapat menambahkan bidang lain sesuai kebutuhan
         };
 
@@ -22,3 +22,38 @@ export const createOrder = async (req, res) => {
     }
 };
 
+
+export const updateStatusOrder = async (req, res) => {
+    try {
+        // Dapatkan orderId terbaru dari database
+        const latestOrder = await Order.findOne({
+            order: [['orderId', 'DESC']], // Ambil order terbaru berdasarkan tanggal pembuatan
+            attributes: ['orderId'] // Hanya ambil orderId saja
+        });
+
+        if (!latestOrder) {
+            return res.status(404).json({ message: 'No orders found' });
+        }
+
+        const orderId = latestOrder.orderId; // Gunakan orderId terbaru
+
+        const { newStatus } = req.body;
+
+        // Cari order berdasarkan orderId
+        const order = await Order.findByPk(orderId);
+
+        if (!order) {
+            return res.status(404).json({ message: 'Order not found' });
+        }
+
+        // Update status order
+        order.orderStatus = newStatus;
+        await order.save();
+
+        // Kirim respons berhasil
+        res.status(200).json({ message: 'Order status updated successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
