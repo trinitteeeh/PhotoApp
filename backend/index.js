63f84bb189b2b51276c36c8ac65686778617fec7
JainsL <117689545+JainsL@users.noreply.github.com>
2024-05-20 12:28:15 +0700

    Img backend

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import ProductRoute from "./route/ProductRoute.js";
 import OrderRoute from "./route/OrderRoute.js";
 import PaymentRoute from "./route/PaymentRoute.js";
+import PhotoRoute from "./route/PhotoRoute.js"
+
 
 const app = express();
 
@@ -12,6 +14,8 @@ app.use(express.json());
 app.use(ProductRoute);
 app.use(OrderRoute);
 app.use("/api/payment", PaymentRoute);
+app.use(PhotoRoute);
+
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server Up And Running on port ${PORT}...`));
